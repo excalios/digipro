1a578c13298062f43a2ac7f83a01a2adcfcc8e82
agunawanalinur <alinuragung@gmail.com>
2022-08-13 11:11:31 +0800

    tes

---
+++ app/src/main/java/com/example/digipro/emergency_call.java
@@ -0,0 +1,14 @@
+package com.example.digipro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class emergency_call extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emergency_call);
+    }
+}
