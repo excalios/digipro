aa63bd5f0a49f3714fbee307ef5b9138f7f27264
Ravi Deevan <ravideevan3@gmail.com>
2022-08-16 07:26:17 +0800

    Fix Errors and Add Auth

--- app/src/main/java/com/example/digipro/MainActivity2.java
+++ app/src/main/java/com/example/digipro/MainActivity2.java
@@ -2,7 +2,25 @@ package com.example.digipro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.android.material.textfield.TextInputEditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +28,88 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        SharedPreferences prefs;
+        SharedPreferences.Editor edit;
+        prefs = getSharedPreferences("myPrefs", Context.MODE_PRIVATE);
+        edit = prefs.edit();
+
+        EditText emailText = findViewById(R.id.sign_email);
+        TextInputEditText passText = findViewById(R.id.pass);
+        Button btn = findViewById(R.id.btn_login);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                JSONObject jsonBody = new JSONObject();
+                try {
+                    jsonBody.put("email", emailText.getText().toString());
+                    jsonBody.put("password", passText.getText().toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                JsonObjectRequest jsonRequest = new JsonObjectRequest(
+                        Request.Method.POST,
+                        "https://digitalprotect.id/api/v1/auth/login",
+                        jsonBody,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                // Display the first 500 characters of the response string.
+                                try {
+                                    String saveToken=response.getString("token");
+                                    edit.putString("token",saveToken);
+                                    Log.i("Login",saveToken);
+                                    edit.commit();
+                                }
+                                catch (JSONException e)
+                                {
+                                    e.printStackTrace();
+                                }
+
+                                Intent i = new Intent(MainActivity2.this, ActivityHome.class);
+                                startActivity(i);
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        String json = null;
+                        NetworkResponse response = error.networkResponse;
+                        if(response != null && response.data != null){
+                            json = new String(response.data);
+                            json = trimMessage(json, "message");
+                            if(json != null) displayMessage(json);
+                        }
+                    }
+                });
+
+                VolleySingleton.getInstance(MainActivity2.this).addToRequestQueue(jsonRequest);
+            }
+        });
+    }
+
+    public String trimMessage(String json, String key){
+        String trimmedString = null;
+
+        try{
+            JSONObject obj = new JSONObject(json);
+            trimmedString = obj.getString(key);
+        } catch(JSONException e){
+            e.printStackTrace();
+            return null;
+        }
+
+        return trimmedString;
+    }
+
+    //Somewhere that has access to a context
+    public void displayMessage(String toastString){
+        Toast.makeText(this, toastString, Toast.LENGTH_LONG).show();
+    }
+
+    public void goToRegisterPage(View view){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }
